b1ecbbb067b977e62d38c09fc3c4869046081b72
Daniel Furniss <30766016+bacon42@users.noreply.github.com>
2023-01-10 23:45:29 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,29 @@
+print('ex 1.')
+
+print(False>True) # Should print False
+
+print('ex 2.')
+
+print(False or(0 != 0 or True)) # Should print True
+
+print('ex 3.')
+
+print(5%2)
+
+print('ex 4.')
+print(not ("testing" == "testing" and "Mario" == "Cool Guy")) # Should print True
+
+print('ex 5.')
+firstName = "Mario"
+lastName = "Rossi"
+sentence = ('Mario '+'Rossi')
+print(sentence) # Should print "Mario Rossi"
+
+print('ex 6.')
+brands = ["Adidas", "Nike"]
+print("Nike" in brands) # Should print True
+
+print('ex 7.')
+brands = ["Adidas", "Nike"]
+
+print(not("Reebok" in brands)) # Should print True
